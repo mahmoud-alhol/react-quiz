7b848dd5b7265760dac52983b6bbe705d5a8ab7c
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-25 10:22:27 +0300

    renamed components to .jsx. created useReducer and useEffect to handle fetching and its status

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready"}
+      </Main>
+    </div>
+  );
+}
